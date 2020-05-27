d74b7bd53c106c4ef141b464f34f87d0b7065213
emmagimsal <emmagisal@gmail.com>
2020-05-27 17:24:48 -0300

    proyecto Maven

---
+++ CodigoJenkins/src/main/java/com/utn/codigojenkins/main.java
@@ -0,0 +1,39 @@
+/*
+ * Emmanuel Gimenez
+ * Legajo: 42632
+ */
+package com.utn.codigojenkins;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Emmanuel Gimenez
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          
+          String[] dna = {"ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"};
+        //String[] dna ={"ATGCGA","AAGTGC","ATATGT","AGAAGT","ACTCGA","ATCACT"};
+        //String[] dna = {"ATGCGA", "TAGTGC", "ATATGT", "AGGAGT", "ACTCAC", "ATCACA"};
+        //String[] dna = {"CTGCTA","TCTTAG","ACCATC","CAACGT","GATCCG","ACCCTC"};
+        //String[] dna ={"ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"}; 
+        //String[] dna ={"GAAGGA","AGAAGG","TATAGT","ATATGT","AAAGTG","AGCAGT"};
+        //String[] dna ={"ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG"};
+        
+        AnalizadorADN analizadorDeADN = new AnalizadorADN();
+         System.out.println("La secuencia es: "+ Arrays.toString(dna));
+        if (analizadorDeADN.isMutant(dna)) {
+            System.out.println("Es mutante");
+        } else {
+            System.out.println("No es mutante");
+        }
+
+        }
+    }
+    
+
